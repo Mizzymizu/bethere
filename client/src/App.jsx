b4128a5d5db8cc31f55dd865253359844e293a95
dreamsalotl <donovanw.walker@gmail.com>
2023-11-09 07:28:21 -0600

    added dashboard and event creation page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { Dashboard, Home, Login, Signup } from './pages';
+import { Dashboard, Home, Login, Signup, CreateEvent } from './pages';
 
 function App() {
   return (
@@ -12,6 +12,8 @@ function App() {
             <Route path="/" element={<Home />} />
             <Route path="/signup" element={<Signup />} />
             <Route path="/login" element={<Login />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/create-event" element={<CreateEvent />} />
           </Routes>
         </header>
       </div>
