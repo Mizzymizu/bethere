578e2115e990c26a32cbcd16096965e7eb47a27c
angiecruz02 <angelicacruz02@hotmail.com>
2023-11-09 19:42:29 -0600

    add logo to home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Logo from '../assets/bethere.png';
 
 const Home = () => {
   return (
     <div>
+      <img src={Logo} alt="BeThere Logo" style={{ width: '300px', }} />
       <h1>Welcome to our RSVP Application</h1>
       <p>
         <Link to="/signup">Sign Up</Link>
