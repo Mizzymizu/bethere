b44a17a02bf4d59f69acda6989c0fed95ab2ae59
angiecruz02 <angelicacruz02@hotmail.com>
2023-11-09 18:36:07 -0600

    event messages

--- client/src/components/EventMessages.jsx
+++ client/src/components/EventMessages.jsx
@@ -1,2 +1,16 @@
 import react from 'react';
 
+const EventMessages = (props) => {
+    return (
+        <div>
+        <div className="event-messages">
+            <div className="event-message">
+            <div className="event-message__user">User</div>
+            <div className="event-message__text">Text</div>
+            </div>
+        </div>
+        </div>
+    );
+    }
+
+export default EventMessages;
