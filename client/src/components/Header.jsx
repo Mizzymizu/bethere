c3dc93139e2fea4d5f9c38f72ac7a4480cc32ccf
angiecruz02 <angelicacruz02@hotmail.com>
2023-11-09 19:28:13 -0600

    displaying the logo in the header

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from '../assets/bethere.png';
+
+const Header = () => {
+    return (
+        <header>
+            <img src={logo} alt="Website Logo" />
+            {}
+        </header>
+    );
+};
+
+export default Header;
