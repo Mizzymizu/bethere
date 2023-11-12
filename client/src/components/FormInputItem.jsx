09e18cb2b07a3a66d08e3b62af51a62c1bc272b9
angiecruz02 <angelicacruz02@hotmail.com>
2023-11-11 22:06:55 -0600

    update RSVP form and styling

--- client/src/components/FormInputItem.jsx
+++ client/src/components/FormInputItem.jsx
@@ -1,8 +1,33 @@
 import React from "react";
-import { Form } from "react-router-dom";
 import PropTypes from "prop-types";
 
-const FormInputItem = ({ title, name, onChange, type, placeholder }) => {
+const FormInputItem = ({
+  title,
+  name,
+  onChange,
+  type,
+  placeholder,
+  options,
+}) => {
+  const getRadioButtons = (options) => {
+    console.log(options);
+    return options.map((option) => (
+      <label key={option} className="form-radio-item">
+        <input type="radio" name={name} value={option} onChange={onChange} />
+        {option}
+      </label>
+    ));
+  };
+
+  if (type === "radio") {
+    return (
+      <label className="form-radio-label">
+        
+        {title}:
+        <div className="form-radio-item-parent">{getRadioButtons(options)}{" "}</div>
+      </label>
+    );
+  }
   return (
     <label className="form-item">
       {title}:
@@ -26,6 +51,7 @@ FormInputItem.propTypes = {
   onChange: PropTypes.func.isRequired,
   type: PropTypes.string.isRequired,
   placeholder: PropTypes.string,
+  options: PropTypes.arrayOf(PropTypes.string),
 };
 
 export default FormInputItem;
--- client/src/pages/RSVP.jsx
+++ client/src/pages/RSVP.jsx
@@ -1,17 +1,43 @@
 import React, { useState } from 'react';
 import FormInputItem from '../components/FormInputItem';
-import { Form } from 'react-router-dom';
+
 
 
 const RSVP = () => {
+  const [formState, setFormState] = useState({
+    name: "",
+    email: "",
+    notes: "",
+    rsvp: "",
+    });
+
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+        setFormState({
+            ...formState,
+            [name]: value,
+        });
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(formState);
+    }
+
     return (
         <div>
         <h1>You are invited</h1>
+        <form onSubmit={handleSubmit} className='input-form'>
+            <h1>RSVP</h1>
+            <h2>Event Details</h2>
+            <FormInputItem title="Name" name="name" onChange={handleChange} type="text" />
+            <FormInputItem title="Email" name="email" onChange={handleChange} type="email" />
+            <FormInputItem title="RSVP" name="rsvp" onChange={handleChange} type="radio" options={["Yes", "No", "Maybe"]} />
+            <FormInputItem title="Notes" name="notes" onChange={handleChange} type="textarea" />
+            <input type="submit" value="RSVP" />
+            </form>
         </div>
     );
-    }
-
-    
-
+    };
 
 export default RSVP;
