6034775f674c165ed53dea5ce25d7952e28a1221
dreamsalotl <donovanw.walker@gmail.com>
2023-11-14 20:23:35 -0600

    added signout

--- client/src/utils/auth.js
+++ client/src/utils/auth.js
@@ -1,48 +1,42 @@
-import decode from 'jwt-decode';
+import jwt_decode from 'jwt-decode';
 
 class AuthService {
-getProfile() {
-    return decode(this.getToken());
-}
+  getProfile() {
+    return jwt_decode(this.getToken());
+  }
 
-loggedIn() {
+  loggedIn() {
     const token = this.getToken();
     return !!token && !this.isTokenExpired(token);
-}
+  }
 
-isTokenExpired(token) {
+  isTokenExpired(token) {
     try {
-        const decoded = decode(token);
-        console.log('Decoded token:', decoded);
-
-        if (decoded.exp < Date.now() / 1000) {
-            console.log('Token is expired');
-            return true;
-        } else {
-            console.log('Token is not expired');
-            return false;
-        }
-    } catch (err) {
-        console.error('Error decoding token:', err);
+      const decoded = jwt_decode(token);
+      if (decoded.exp < Date.now() / 1000) {
+        return true;
+      } else {
         return false;
+      }
+    } catch (err) {
+      return false;
     }
-}
+  }
 
-
-getToken() {
+  getToken() {
     return localStorage.getItem('id_token');
-}
+  }
 
-login(idToken) {
+  login(idToken) {
     // Saves user token to localStorage
     localStorage.setItem('id_token', idToken);
     window.location.assign('/');
-}
+  }
 
-logout() {
+  logout() {
     localStorage.removeItem('id_token');
     window.location.assign('/');
-}
+  }
 }
 
 export default new AuthService();
