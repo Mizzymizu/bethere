3763d91e1ba60bcd067971aa9866cc7d40744858
angiecruz02 <angelicacruz02@hotmail.com>
2023-11-15 11:37:45 -0600

    add style to the home page and login page

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -70,20 +70,21 @@ function Login() {
       <form onSubmit={handleSubmit}>
         <label>
           Email:
-          <input type="email" name="email" onChange={handleChange} />
+          <input type="email" name="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" onChange={handleChange} />
         </label>
         <label>
-          Password:
+         Password:
           <input
             type={showPassword ? 'text' : 'password'} // Toggle between text and password
             name="password"
             onChange={handleChange}
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
           />
           <button type="button" onClick={handleTogglePassword}>
             {showPassword ? 'Hide' : 'Show'}
           </button>
         </label>
-        <button type="submit">Log In</button>
+        <button type="submit" className="mt-5 text-white bg-green-700 hover:bg-green-800 focus:outline-none focus:ring-4 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2  dark:bg-green-600 dark:hover:bg-green-700  dark:focus:ring-green-800">Log In</button>
       </form>
     </div>
   );
