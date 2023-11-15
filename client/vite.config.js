e0b22b770da384ed7de273aa69d4f5b25935d95d
dreamsalotl <donovanw.walker@gmail.com>
2023-11-14 18:25:29 -0600

    fixed script issue in html

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,16 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
-// vite.config.js
 export default defineConfig({
+  main: 'index.html',
   plugins: [react()],
   server: {
     port: 3000,
     open: true,
     proxy: {
       '/graphql': {
-        target: 'http://127.0.0.1:3001', // Updated target to use IPv4 loopback address
+        target: 'http://127.0.0.1:3001',
         secure: false,
         changeOrigin: true,
       },
